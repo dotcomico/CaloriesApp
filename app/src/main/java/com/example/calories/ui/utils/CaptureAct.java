61cf794cef8bd37bc5f113d07c0356d0325c0d58
dotan <dotcompanyofficial@gmail.com>
2025-07-28 13:01:20 +0300

    CaptureAct - add scanCode() method.  for start barcode scanning operation.

--- app/src/main/java/com/example/calories/ui/utils/CaptureAct.java
+++ app/src/main/java/com/example/calories/ui/utils/CaptureAct.java
@@ -1,7 +1,18 @@
 package com.example.calories.ui.utils;
 
+import android.app.Activity;
+import android.content.Context;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class CaptureAct extends CaptureActivity {
-
+    public void scanCode(Context context) {
+        IntentIntegrator integrator = new IntentIntegrator((Activity) context);
+        integrator.setCaptureActivity(CaptureAct.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("לחץ על מקשי צליל להפעלת פנס");
+        integrator.initiateScan();
+    }
 }
