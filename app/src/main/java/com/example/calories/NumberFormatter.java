f15724dc2d492bea2ed5eb26377523897c28055f
dotan <dotcompanyofficial@gmail.com>
2025-10-08 02:07:25 +0300

    feat: Introduce NumberFormatter utility
    This commit introduces a new `NumberFormatter` utility class with a static `formatAmount` method.
    
    This method formats a `double` value into a `String`. If the number is a whole number (e.g., 10.0), it is formatted as an integer ("10"). If it has a fractional part (e.g., 10.5), it is returned as a string representation of the double ("10.5").

---
+++ app/src/main/java/com/example/calories/NumberFormatter.java
@@ -0,0 +1,15 @@
+package com.example.calories;
+
+public class NumberFormatter {
+
+        public static String formatAmount(double number) {
+            // Check if the number has no fractional part
+            if (number == (long) number) {
+                // It's a whole number, format as an integer
+                return String.format("%d", (long) number);
+            } else {
+                // It has a decimal part, return as is (or you can use DecimalFormat for more control)
+                return String.valueOf(number);
+            }
+        }
+}
